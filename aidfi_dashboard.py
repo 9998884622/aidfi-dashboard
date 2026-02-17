57b8c412be8ed3a3a65e5337ebab1cef41e55bf2
9998884622 <priya1722006@gmail.com>
2026-02-17 15:59:56 +0530

    aidfi_dashboard.py

--- aidfi_dashboard.py
+++ aidfi_dashboard.py
@@ -1,20 +1,35 @@
 import streamlit as st
 
+st.set_page_config(page_title="AIDFI Cyber", layout="wide")
+
 st.title("🛡 AIDFI – AI Digital Forensics Investigator")
 
-file = st.file_uploader("Upload Log File")
+st.sidebar.title("Control Panel")
+
+file = st.sidebar.file_uploader("Upload Log File")
 
 if file:
+
     data = file.read().decode()
 
+    st.subheader("Investigation Dashboard")
+
     if "failed" in data.lower():
-        st.error("⚠ Failed Login Attempts Detected")
-        st.write("AI Conclusion: Possible Brute Force Attack")
+
+        st.error("🔴 Threat Level: HIGH")
+
+        st.write("Attack Type: Brute Force Attack")
+
+        st.write("AI Investigator: Multiple login failures detected")
 
     elif "unauthorized" in data.lower():
-        st.error("⚠ Unauthorized Access Detected")
+
+        st.warning("🟠 Threat Level: MEDIUM")
 
     else:
-        st.success("✅ No Threat Found")
 
-    st.download_button("Download Report", "AIDFI Report Generated")
+        st.success("🟢 Threat Level: SAFE")
+
+    report = "AIDFI Cyber Forensic Report Generated"
+
+    st.download_button("Download Report", report)
