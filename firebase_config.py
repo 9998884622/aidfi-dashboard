3126991ffff5defdf1065007b23a9c195e9c12fa
9998884622 <priya1722006@gmail.com>
2026-02-19 11:36:02 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,67 +1,26 @@
+
+Delete everything.
+
+Paste this ONLY:
+
 ```python
-import firebase_admin
-
-from firebase_admin import credentials,storage,db
-
 import streamlit as st
-
+import firebase_admin
+from firebase_admin import credentials, storage, db
 
 if not firebase_admin._apps:
 
-    cred=credentials.Certificate(dict(st.secrets["FIREBASE"]))
-
-    firebase_admin.initialize_app(
-
-        cred,
-
-        {
-
-        "storageBucket":st.secrets["FIREBASE"]["project_id"]+".appspot.com",
-
-        "databaseURL":"https://"+st.secrets["FIREBASE"]["project_id"]+"-default-rtdb.firebaseio.com/"
-
-        }
-
-    )
-
-
-bucket=storage.bucket()
-
-ref=db.reference("intruder")
-
-
-def upload_image(path,name):
-
-    blob=bucket.blob(name)
-
-    blob.upload_from_filename(path)
-
-
-def save_location(user,lat,lng):
-
-    ref.child(user).set({
-
-    "lat":lat,
-
-    "lng":lng
-
+    cred = credentials.Certificate({
+        "type": st.secrets["FIREBASE_TYPE"],
+        "project_id": st.secrets["FIREBASE_PROJECT_ID"],
+        "private_key_id": st.secrets["FIREBASE_PRIVATE_KEY_ID"],
+        "private_key": st.secrets["FIREBASE_PRIVATE_KEY"],
+        "client_email": st.secrets["FIREBASE_CLIENT_EMAIL"],
+        "client_id": st.secrets["FIREBASE_CLIENT_ID"],
+        "token_uri": st.secrets["FIREBASE_TOKEN_URI"]
     })
 
-
-def get_images():
-
-    blobs=bucket.list_blobs()
-
-    urls=[]
-
-    for blob in blobs:
-
-        urls.append(blob.public_url)
-
-    return urls
-
-
-def get_locations():
-
-    return ref.get()
-```
+    firebase_admin.initialize_app(cred, {
+        "storageBucket": "aidfi-492df.appspot.com",
+        "databaseURL": "https://aidfi-492df-default-rtdb.firebaseio.com/"
+    })
