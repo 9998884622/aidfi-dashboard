939d527be62126f2ba41736c790fa8b006a1d8be
9998884622 <priya1722006@gmail.com>
2026-02-19 12:37:51 +0530

    Update 4_Threat_Analysis.py

--- pages/4_Threat_Analysis.py
+++ pages/4_Threat_Analysis.py
@@ -3,12 +3,31 @@ import firebase_config as fb
 
 st.title("Threat Analysis")
 
-file=st.file_uploader("Upload File")
+# check login
+if "user" not in st.session_state:
 
-if st.button("Scan"):
+    st.warning("Please Login First")
 
-    if file:
+else:
 
-        st.error("Threat Detected")
+    file = st.file_uploader("Upload File")
 
-        fb.upload_file(file.name,file.name)
+    lat = st.text_input("Enter Latitude")
+
+    lng = st.text_input("Enter Longitude")
+
+    if st.button("Scan"):
+
+        if file:
+
+            # save file
+            fb.upload_file(file.name, file.name)
+
+            # save location
+            if lat and lng:
+
+                fb.save_location(st.session_state.user, lat, lng)
+
+            st.error("Threat Detected")
+
+            st.success("Saved Successfully")
